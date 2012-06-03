a0356db40efdfcce867a2488825db2e359eeea33
amarnath <mailamars@gmail.com>
2012-06-03 17:18:12 +0530

    10 commit

--- db/migrate/20120603113637_change_phone_type_friend.rb
+++ db/migrate/20120603113637_change_phone_type_friend.rb
@@ -2,10 +2,12 @@ class ChangePhoneTypeFriend < ActiveRecord::Migration
   def self.up
 	  change_table :friends do |t|
          t.change :phone_number, :bigint
+	 end
   end
 
   def self.down
 	   change_table :friends do |t|
      	t.change :phone_number, :integer
+	end
   end
 end
