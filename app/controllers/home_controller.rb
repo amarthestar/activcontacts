518db013876f2e1c3008563ecc48490b16d4613f
amarnath <mailamars@gmail.com>
2012-06-05 11:40:23 +0530

    16 commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,13 @@
 class HomeController < ApplicationController
   def index
-     @mysearch=Mysearch.find(:all, :order => "id desc", :limit => 5)
-     puts @mysearch.inspect
+     @mysearch=Mysearch.find(:all, :order => "id desc", :limit => 5)#taking last five records for mysearches table & displying in the home page
   end
   
   def aboutus
   end
    
   def search
-  p params[:name]
-  if @user=User.find_by_email(params[:name])
+  if @user=User.find_by_email(params[:name])#getting the params(name or email) as :name from the search field and finding in db and put in user instance variable
   else puts @user=User.find_by_phone_number(params[:name])
    end
   end
