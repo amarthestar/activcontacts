aa789cbef05b74b483b50757c31dad27074f56c8
amarnath <mailamars@gmail.com>
2012-06-08 12:44:45 +0530

    commit13

--- app/models/activcontactsphone.rb
+++ app/models/activcontactsphone.rb
@@ -1,5 +1,4 @@
 class Activcontactsphone < ActiveRecord::Base
 belongs_to :user
-attr_reader :status
-attr_accessor :status
+
 end
--- app/models/myactivcontact.rb
+++ app/models/myactivcontact.rb
@@ -1,6 +1,3 @@
 class Myactivcontact < ActiveRecord::Base
  belongs_to :user
-attr_reader :status
-attr_accessor :status
-
 end
