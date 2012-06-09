4414c09cefe42200b976f323b2c6a50c79200219
amarnath <mailamars@gmail.com>
2012-06-09 17:41:45 +0530

    commit20

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,9 +1,9 @@
 class Friend < ActiveRecord::Base
 belongs_to :user
-  validates :first_name, :format=>{:with =>/^[a-zA-Z\-' ]+$/, :message => "First name can only contain letters"}
+  validates :first_name, :format=>{:with =>/^[a-zA-Z\-' ]+$/, :message => "can only contain letters"}
   validates :first_name, :length=>{:in=>2..30, :message => "Please enter First name 2-30 characters"}
-  validates :last_name, :format=>{:with =>/^[a-zA-Z\-' ]+$/, :message => "Last name can only contain letters"}
+  validates :last_name, :format=>{:with =>/^[a-zA-Z\-' ]+$/, :message => "can only contain letters"}
   validates :last_name, :length=>{:in=>2..30, :message => "Please enter Last name 2-30 characters"}
-  validates :nick_name, :format=>{:with =>/^[a-zA-Z\-' ]+$/, :message => "Nick name can only contain letters"}
+  validates :nick_name, :format=>{:with =>/^[a-zA-Z\-' ]+$/, :message => "can only contain letters"}
   validates :nick_name, :length=>{:in=>2..30, :message => "Please enter Nick name 2-30 character"}
 end
--- app/models/user.rb~
+++ app/models/user.rb~
@@ -9,9 +9,9 @@ class User < ActiveRecord::Base
    has_many :messages
    has_many :mysearches
   # Setup accessible (or protected) attributes for your model
-  validates :first_name, :format=>{:with =>/^[a-zA-Z\-' ]+$/, :message => "First name can only contain letters"}
+  validates :first_name, :format=>{:with =>/^[a-zA-Z\-' ]+$/, :message => "can only contain letters"}
   validates :first_name, :length=>{:in=>2..30, :message => "Please enter first name (2-30 characters)"}
-  validates :last_name, :format=>{:with =>/^[a-zA-Z\-' ]+$/, :message => "Last name can only contain letters"}
+  validates :last_name, :format=>{:with =>/^[a-zA-Z\-' ]+$/, :message => "can only contain letters"}
   validates :last_name, :length=>{:in=>2..30, :message => "Please enter Last name (2-30 characters)"}
 
 
