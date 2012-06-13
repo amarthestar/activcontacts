40254415a16ddee62989dc96d4e615142a53d096
amarthestar <mailamars@gmail.com>
2012-06-13 18:04:36 +0530

    Update master

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :enable_starttls_auto => true,
   :user_name => "amarnaths1985@gmail.com",             
-  :password => 'salem1234'
+  :password => '########'
   }
   
 end
